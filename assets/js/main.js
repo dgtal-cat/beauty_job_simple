f4feab0db047f39f1bdca638b7074a50ca6bd7f2
Dgtal_Cat <dgtal.cat@gmail.com>
2018-08-03 19:51:47 +0300

    i don't understand this sliders...

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,23 @@ var block = document.getElementsByClassName('slider_text')[0];
 
 var initialText = block.innerHTML;
 
-function changeText () {
-    console.log(block.innerHTML);
+var slideList = document.getElementsByClassName('slide_item');
+console.log(slideList);
+
+function changeSlide() {
+    for (i=0; i<slideList.length; i++) {
+        console.log(slideList[i].style.left);
+        console.log(slideList[i]);
+        slideList[i].style.left = '-100%';
+    }
+}
+
+/*function changeText () {
     if (block.innerHTML !== 'pshe') {
     block.innerHTML = 'pshe';
     } else {
         block.innerHTML = initialText;
     }
-}
+}*/
 
-block.onclick = changeText;
+block.onclick = changeSlide;
