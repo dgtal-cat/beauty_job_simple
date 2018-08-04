3cb42c1f5447d57650019de22075b919afe5b446
dgtal-cat <dgtal.cat@gmail.com>
2018-08-04 23:59:29 +0300

    delete jquery.js and continue with slider visual counters

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,15 @@ const nextButton = document.getElementsByClassName('slide_changer_next')[0];
 const prevButton = document.getElementsByClassName('slide_changer_prev')[0];
 const slidesList = document.getElementsByClassName('slides_list')[0];
 const maxIndex = document.getElementsByClassName('slide_item').length;
+const sliderCounterActive = document.getElementsByClassName('slider_counter_active');
+const sliderCounterAverage = document.getElementsByClassName('slider_counter_average');
 
 let index = 1;
+for (i=0;i<sliderCounterActive.length;i++) {
+    sliderCounterActive[i].innerHTML = '0' + index;
+    sliderCounterAverage[i].innerHTML = '0' + maxIndex;
+}
+
 
 console.log(index);
 console.log(maxIndex);
@@ -16,6 +23,9 @@ nextButton.onclick = function () {
         slidesList.style.left = '0';
         index = 1;
     }
+    for (i=0;i<sliderCounterActive.length;i++) {
+        sliderCounterActive[i].innerHTML = '0' + index;
+    }
     console.log('after click on NEXT index = ' + index);
 };
 
@@ -27,5 +37,8 @@ prevButton.onclick = function () {
     }
     slidesList.style.left = '-' + index + '00%';
     index = index + 1;
+    for (i=0;i<sliderCounterActive.length;i++) {
+        sliderCounterActive[i].innerHTML = '0' + index;
+    }
     console.log('after click on PREV index = ' + index);
 };
