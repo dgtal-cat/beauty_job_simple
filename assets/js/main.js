f20bb77328c0b82e61426aaed197a6c8d450bd77
dgtal-cat <dgtal.cat@gmail.com>
2018-08-04 23:42:03 +0300

    done with slider (yoopeeee)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,29 @@ const prevButton = document.getElementsByClassName('slide_changer_prev')[0];
 const slidesList = document.getElementsByClassName('slides_list')[0];
 const maxIndex = document.getElementsByClassName('slide_item').length;
 
-let index = 0;
+let index = 1;
+
 console.log(index);
 console.log(maxIndex);
 
 nextButton.onclick = function () {
     if (index < maxIndex) {
-        index = index + 1;
         slidesList.style.left = '-' + index + '00%';
+        index = index + 1;
     } else {
-        index = 0;
+        slidesList.style.left = '0';
+        index = 1;
     }
     console.log('after click on NEXT index = ' + index);
 };
 
 prevButton.onclick = function () {
-    if (index = 1) {
-        index = maxIndex-1;
-        slidesList.style.left = '-' + index + '00%';
+    if (index == 1) {
+        index = maxIndex - 1;
     } else {
-        index = index-1;
-        slidesList.style.left = '-' + index + '00%';
+        index = index - 2;
     }
+    slidesList.style.left = '-' + index + '00%';
+    index = index + 1;
     console.log('after click on PREV index = ' + index);
 };
